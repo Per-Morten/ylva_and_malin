79d96191b0a196e7e3f56c043625400ab9cf6f40
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-11-11 20:59:27 +0100

    fix #41: Fix internal compiler error on windows
    Fixed the internal compiler error that I got when compiling on windows.
    It was related to a missing implementation of built-in expects.

--- src/ym/core/ym_attributes.h
+++ src/ym/core/ym_attributes.h
@@ -1,4 +1,22 @@
 #pragma once
+// Setup checks for builtin features, attributes and extensions.
+// See: https://clang.llvm.org/docs/LanguageExtensions.html
+
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+
+#ifndef __has_extension
+#define __has_extension __has_feature
+#endif
+
+#ifndef __has_attribute
+#define __has_attribute(x) 0
+#endif
 
 ///////////////////////////////////////////////////////////
 /// \ingroup ym_core
@@ -7,7 +25,11 @@
 ///     YM_INLINE is a platform independent macro
 ///     that forces a function to be inlined.
 ///////////////////////////////////////////////////////////
+#if __has_attribute(always_inline)
 #define YM_INLINE __attribute__((always_inline)) inline
+#else
+#define YM_INLINE
+#endif
 
 ///////////////////////////////////////////////////////////
 /// \ingroup ym_core
@@ -16,7 +38,11 @@
 ///     YM_NO_INLINE is a platform independent macro
 ///     that forces a function to never be inlined.
 ///////////////////////////////////////////////////////////
+#if __has_attribute(noinline)
 #define YM_NO_INLINE __attribute__((noinline))
+#else
+#define YM_NO_INLINE
+#endif
 
 ///////////////////////////////////////////////////////////
 /// \ingroup ym_core
@@ -25,7 +51,11 @@
 ///     YM_PURE is a platform independent macro
 ///     informing the compiler that a function is pure.
 ///////////////////////////////////////////////////////////
+#if __has_attribute(pure)
 #define YM_PURE __attribute__((pure))
+#else
+#define YM_PURE
+#endif
 
 ///////////////////////////////////////////////////////////
 /// \ingroup ym_core
@@ -44,7 +74,11 @@
 ///     informing the compiler that a function/variable
 ///     is intentionally unused.
 ///////////////////////////////////////////////////////////
+#if __has_attribute(unused)
 #define YM_UNUSED __attribute__((unused))
+#else
+#define YM_UNUSED
+#endif
 
 ///////////////////////////////////////////////////////////
 /// \ingroup ym_core
@@ -54,7 +88,11 @@
 ///     informing the compiler to warn if a return
 ///     value from a function is discarded.
 ///////////////////////////////////////////////////////////
+#if __has_attribute(warn_unused_result)
 #define YM_NO_DISCARD __attribute__((warn_unused_result))
+#else
+#define YM_NO_DISCARD
+#endif
 
 ///////////////////////////////////////////////////////////
 /// \ingroup ym_core
@@ -64,7 +102,12 @@
 ///     informing the compiler that in a branch
 ///     this is the path that is most likely.
 ///////////////////////////////////////////////////////////
+// Having issues with this in release on windows.
+#if __has_builtin(__builtin_expect) && !defined(WIN32)
 #define YM_LIKELY(x) __builtin_expect(!!(x), 1)
+#else
+#define YM_LIKELY(x) (x)
+#endif
 
 ///////////////////////////////////////////////////////////
 /// \ingroup ym_core
@@ -74,5 +117,10 @@
 ///     informing the compiler that in a branch
 ///     this is the path that is least likely.
 ///////////////////////////////////////////////////////////
+// Having issues with this in release on windows.
+#if __has_builtin(__builtin_expect) && !defined(WIN32)
 #define YM_UNLIKELY(x) __builtin_expect(!!(x), 0)
+#else
+#define YM_UNLIKELY(x) (x)
+#endif
 
