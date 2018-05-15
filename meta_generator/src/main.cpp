f78c4ac09da0fdb611d8516f812f41cd4782318b
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2018-05-15 18:56:18 +0200

    Start experimenting with filesystem

--- meta_generator/src/main.cpp
+++ meta_generator/src/main.cpp
@@ -1,6 +1,37 @@
+#include <meta.h>
+#include <logger.h>
+#include <string>
+#include <iostream>
+#include <experimental/filesystem>
 
+namespace fs = std::experimental::filesystem;
 
-int main(int argc, char** argv)
+// input:
+//  directory_name
+//  cache_path(?)
+//  output_directory
+// output:
+//  .h files,
+//  .cpp files,
+//  CMakeLists.txt
+
+// Parallelize this by giving each thread a top directory
+int
+main(int argc, char** argv)
 {
+    auto env = std::getenv("YM_SOURCE_DIR");
+
+    for(auto& p: fs::recursive_directory_iterator(env))
+        printf("%s\n", p.path().c_str());
+        //std::cout << p << '\n';
+
+
+    // if (env)
+    //     LOG_DEBUG("Yay: %s", env);
+
+    // std::string path = "../../src/ym/";
+    // std::string command = "cd " + path;
+    // system(path.c_str());
+
     return 0;
 }
