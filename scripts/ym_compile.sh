04d2348dc13f97dd04dd98763ef19b2894d73268
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2018-06-09 11:54:06 +0200

    ym_compile: update ym_compile works on ubuntu

--- scripts/ym_compile.sh
+++ scripts/ym_compile.sh
@@ -1,99 +1,5 @@
 #!/bin/bash
 
-# ym_internal_compile()
-# {
-#     local windows_toolset=""
-#     if [[ "$(uname)" ==  "MINGW64_NT-10.0" ]];
-#     then
-#         windows_toolset=-T"v140_clang_c2"
-#     fi
-
-#     cmake ${windows_toolset} -DCMAKE_BUILD_TYPE=${1} ${YM_SOURCE_DIR}
-
-#     if [[ "$(uname)" != "MINGW64_NT-10.0" ]];
-#     then
-#         make "-j8";
-#     fi
-# }
-
-# ym_internal_print_compile_options()
-# {
-#     printf "Compilation options:"
-#     printf "\n\t- debug"
-#     printf "\n\t- release"
-#     printf "\n\t- test"
-#     printf "\n\t- rebuild | clean"
-#     printf "\n\n\n"
-# }
-
-# ym_compile()
-# {
-#     pushd ${YM_ROOT_DIR} >/dev/null
-#     local build_type="release"
-#     local run=false
-
-#     # Ugly, fix later
-#     if [[ ${#} -gt 0 ]];
-#     then
-#         if [[ ${1} =~ -r ]];
-#         then
-#             run=true
-
-#             if [[ ${#} -gt 1 ]];
-#             then
-#                 build_type=${2,,}
-#             fi
-#         else
-#             build_type=${1,,}
-#         fi
-
-#         # Set to lowercase
-#     else
-#         ym_internal_print_compile_options
-#     fi
-
-#     if [[ ${build_type} =~ (rebuild|clean) ]];
-#     then
-#         rm -rf ./build
-#         build_type="release"
-#     fi
-
-#     printf "Building with type: %s\n" ${build_type}
-
-#     if [[ ! -d "build" ]];
-#     then
-#         mkdir build
-#     fi
-#     pushd build >/dev/null
-
-#     # Setup build type dir
-#     if [[ ! -d ${build_type} ]];
-#     then
-#         mkdir ${build_type}
-#     fi
-
-#     pushd ${build_type} >/dev/null
-
-#     # Uppercase first letter
-#     # Also should check if compilation succeeded
-#     # If it didn't we shouldn't run the program
-#     ym_internal_compile ${build_type,,^}
-#     local compile_res=${?}
-
-#     popd >/dev/null # eo build_type dir
-
-#     # Pop out of the rest
-#     popd >/dev/null # eo build dir
-
-#     if [[ ${run} == true && ${compile_res} -eq 0 ]];
-#     then
-#         ./build/${build_type}/bin/main
-#     fi
-#     popd >/dev/null # eo YM_ROOT_DIR
-
-#     return ${compile_res}
-# }
-
 # $0: Build target
 # $1: Build type
 ym_internal_compile()
@@ -223,7 +129,7 @@ ym_continous_game()
             work_focus=$(xprop -root | awk '/_NET_ACTIVE_WINDOW/ {print $5; exit}')
 
             # Launch compilation
-            ym_compile
+            ym_compile -o game -t release
 
             if [[ $? -eq 0 ]];
             then
